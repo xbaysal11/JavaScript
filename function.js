ac33ee5348caeb18ad7bdbbcc524871b9ba2673d
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-02-11 00:08:32 +0600

    Starting function

---
+++ function.js
@@ -0,0 +1,13 @@
+//global var
+var userName = "Flash";
+
+const showMsg = () => {
+  //function var
+  var userName = "Baisalbek";
+  var msg = "Hello " + userName;
+  console.log(msg);
+  console.log(userName);
+};
+//call function
+showMsg();
+console.log(userName);
