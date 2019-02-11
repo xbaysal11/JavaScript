d919ca8cc5015fcdf2ec9d4dc2a35e4504e7b2a0
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-02-11 11:37:09 +0600

    Unary and binary operators

---
+++ unaryBinary.js
@@ -0,0 +1,14 @@
+var apple = "1";
+var banana = "2";
+
+//add string
+var result = apple + banana;
+console.log(result);
+
+//add num
+var resultNum = +apple + +banana;
+console.log(resultNum);
+
+//subtraction num
+var resultSubtr = apple - banana;
+console.log(resultSubtr);
