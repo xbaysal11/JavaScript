181c4e8662980dff4b0b79b961762394d2799d8f
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-02-11 12:11:46 +0600

    increments

--- increment.js
+++ increment.js
@@ -5,3 +5,21 @@ console.log(2 * i++); //use writen before i++ and write new
 console.log(2 * ++i); //write and then use
 
 console.log((i *= 2));
+
+var a = 1,
+  b = 1,
+  c,
+  d;
+
+c = ++a;
+alert(c); // 2
+d = b++;
+alert(d); // 1
+
+c = 2 + ++a;
+alert(c); // 5
+d = 2 + b++;
+alert(d); // 4
+
+alert(a); // 3
+alert(b); // 3
