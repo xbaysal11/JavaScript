65579d32c48e913e950ad8321d1079f00a72b01c
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-02-11 13:29:37 +0600

    comparing String and int

---
+++ compareString.js
@@ -0,0 +1,16 @@
+const compareString = (a, b) => {
+  //splitting into arr
+  var name1 = a.split("");
+  var name2 = b.split("");
+
+  console.log(name1, name2);
+
+  isNaN(a) ? console.log(!(name1 > name2)) : console.log(name1 > name2);
+};
+
+//user input
+var in1 = prompt("1-letter");
+var in2 = prompt("2-letter");
+
+//call funcrtion
+compareString(in1, in2);
